a8b0a97b8a733d750e22c343b80e9efe50e0d240
Ramesh A <ramesharpu@gmail.com>
2026-05-12 11:20:09 +0530

    modified the pom.xml

--- src/test/java/com/hm/hm_demo/HmDemoApplicationTests.java
+++ src/test/java/com/hm/hm_demo/HmDemoApplicationTests.java
@@ -7,7 +7,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 class HmDemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void tc1() {
+	}
+	
+	@Test
+	void tc2() {
+	}
+	@Test
+	void tc3() {
+	}
+	@Test
+	void tc4() {
+	}
+	@Test
+	void tc5() {
 	}
 
 }
